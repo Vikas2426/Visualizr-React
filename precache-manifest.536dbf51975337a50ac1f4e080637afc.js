f5bc836fa4f638f6d90ca2a96067985aa66f841d
Vikas2426 <vikas.cby@gmail.com>
2021-01-17 17:59:24 +0530

    Updates

---
+++ precache-manifest.536dbf51975337a50ac1f4e080637afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ca7e9e18cc21de95fce069e437df70",
+    "url": "/Visualizr-React/index.html"
+  },
+  {
+    "revision": "85db6f17a1e19565b1a0",
+    "url": "/Visualizr-React/static/css/main.c77e0497.chunk.css"
+  },
+  {
+    "revision": "8109ca69ebf0552b4345",
+    "url": "/Visualizr-React/static/js/2.d5869717.chunk.js"
+  },
+  {
+    "revision": "c90c8ae3244f49612816aa7cab780b18",
+    "url": "/Visualizr-React/static/js/2.d5869717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85db6f17a1e19565b1a0",
+    "url": "/Visualizr-React/static/js/main.715f5c08.chunk.js"
+  },
+  {
+    "revision": "b39386539fa1b35f080f",
+    "url": "/Visualizr-React/static/js/runtime-main.1d7f236d.js"
+  }
+]);
